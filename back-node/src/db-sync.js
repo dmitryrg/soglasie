dada75b343f321f971b4260d4c45cd0ebf01f86a
Dmitry Roganov <roganov@pharmhub.ru>
2021-05-28 15:46:15 +0300

    add comments to db-sync

--- back-node/src/db-sync.js
+++ back-node/src/db-sync.js
@@ -1,14 +1,15 @@
 const _ = require('lodash')
 
 function add(dbRows, apiRows) {
+  // в db имена firstName в api first_name, привожу все к firstName / сamelCase
   return apiRows
     .map(apiRow => objKeysCamelCase(apiRow))
     .filter(apiRow => !dbRows.find(dbRow => _.isEqual(apiRow, dbRow)))
 }
 
 function del(dbRows, apiRows) {
+  // в db имена firstName в api first_name, привожу все к firstName / сamelCase
   const _apiRows = apiRows.map(apiRow => objKeysCamelCase(apiRow))
-
   return dbRows.filter(dbRow => !_apiRows.find(apiRow => _.isEqual(apiRow, dbRow)))
 }
 
